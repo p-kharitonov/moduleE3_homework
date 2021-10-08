c1f35f2ccee543703a481b828c86660a8455cdb4
brightside <workingpavel@mail.ru>
2021-10-08 19:54:10 +0300

    task 1

---
+++ task_1.js
@@ -0,0 +1,25 @@
+Задание 1
+function showCountNumber(arr) {
+    let countEven = 0;
+    let countOdd = 0;
+    let countZero = 0;
+    arr.forEach(function(item, index, array) {
+        if (typeof item == "number" && !isNaN(item)) {
+            if (item == 0) {
+                countZero++;
+            }
+            if (item%2 == 0) {
+                countEven++;
+            } else {
+                countOdd++;
+            }
+        }
+    });
+    console.log("Четных: " + countEven);
+    console.log("Не четных: " + countOdd);
+    console.log("Нулей: " + countZero);
+    return null
+}
+
+let arr = [1, 5, 7, "h", true, 10, null, NaN, 0, 4];
+showCountNumber(arr)
